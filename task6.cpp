47f869f091cfc35d4fc3fb34d3eb3f963b468ad3
Arwa <144407619+Arwa119@users.noreply.github.com>
2023-09-12 19:52:21 +0500

    Add files via upload

---
+++ task6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"                           * * *  *                                    "<<endl;
+cout<<"                         *    12    *                                  "<<endl;
+cout<<"                       * 11    !    1 *                                "<<endl;
+cout<<"                      *        !       *                               "<<endl;
+cout<<"                     * 10      !      2 *                              "<<endl;
+cout<<"                    *          !         *                             "<<endl;
+cout<<"                    *  9       o       3 *                             "<<endl;
+cout<<"                    *         /          *                             "<<endl;
+cout<<"                     *  8    /        4  *                             "<<endl;
+cout<<"                      *     /           *                              "<<endl;
+cout<<"                       *   7        5  *                               "<<endl;
+cout<<"                         *     6     *                                 "<<endl;
+cout<<"                            *  *  *                                    "<<endl;
+
+
+
+return 0;
+}
---
+++ task7.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"                                           ___                                                                                      "<<endl;
+cout<<"                                          (___)                                                                                   "<<endl;
+cout<<"                                            |                                                                                    "<<endl;
+cout<<"                                            |                                                                                    "<<endl;
+cout<<"                                            |                                                                                    "<<endl;
+cout<<"                                |||||||||||||||||||||||||                                                                                         "<<endl;
+cout<<"                                |                       |                                                                  "<<endl;
+cout<<"                                |    ___         ___    |                                                                           "<<endl;
+cout<<"                                |   |  O|       |  O|   |                                                                       "<<endl;
+cout<<"                               ||   |___|       |___|   ||                                                                     "<<endl;
+cout<<"                              | |                       | |                                                               "<<endl;
+cout<<"                               ||                       ||                                                                "<<endl;
+cout<<"                                |      ___________      |                                                                             "<<endl;
+cout<<"                                |     |___________|     |            /     \\                                                  "<<endl;
+cout<<"                                 |                     |            (       )                                                "<<endl;
+cout<<"                                  |||||||||||||||||||||              |_____|                                                                        "<<endl;
+cout<<"                                    |               |                  | |                                                "<<endl;
+cout<<"                          |||||||||||||||||||||||||||||||||||||||||    | |                                                                                         "<<endl;
+cout<<"                          |                                       |    | |                                                  "<<endl;
+cout<<"                          |     |||||||||||||||||||||||||||       |    | |                                                                             "<<endl;
+cout<<"                          |     |                         |       |    | |                                                    "<<endl;
+cout<<"                  ________|     |                         |       |____| |                                                        "<<endl;
+cout<<"                 |  ______|     |       MR. ROBO !!!      |       |______|                                                                    "<<endl;
+cout<<"                 |  |     |     |                         |       |                           "<<endl;
+cout<<"                 |  |     |     |||||||||||||||||||||||||||       |                                                                                   "<<endl;
+cout<<"                 |  |     |     |    O    O     O    O    |       |                                                       "<<endl;
+cout<<"                 |  |     |     |    1    2     3    4    |       |                                                         "<<endl;
+cout<<"                _|__|_    |     |||||||||||||||||||||||||||       |                                                                              "<<endl;
+cout<<"               |      |   |                                       |                                                   "<<endl;
+cout<<"              (        )  |_______________________________________|                                                                                          "<<endl;
+cout<<"               \\     /           |  |                 |  |                                                                  "<<endl;
+cout<<"                                  |  |                 |  |                                                             "<<endl;
+cout<<"                                  |  |                 |  |                                                              "<<endl;
+cout<<"                                  |  |                 |  |                                                              "<<endl;
+cout<<"                               ___|  |              ___|  |                                                              "<<endl;
+cout<<"                              |      |             |      |                                                           "<<endl;
+cout<<"                              ^^^^^^^^             ^^^^^^^^                                                                        "<<endl;
+return 0;
+}
